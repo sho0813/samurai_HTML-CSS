62a28f8e3e4303fe7e884410f9609d9f49a211ff
sho0813 <ks4.bsk0813@gmail.com>
2020-10-21 01:02:51 +0900

    contact.html/adds-contact.js contact.js/make function

---
+++ contact.js
@@ -0,0 +1,27 @@
+function check(){
+  if(form.name.value==""){
+      alert("お名前が入力されていません。お名前を入力してください。")
+      return false;
+  }else if(form.email.value==""){
+      alert("メールアドレスが入力されていません。メールアドレスを入力してください。")
+      return false;
+  }else if(form.yearolds.value==""){
+      alert("年齢が入力されていません。年齢を入力してください。")
+      return false;
+  }else if(form.sex.value==""){
+      alert("性別が入力されていません。性別を入力してください。")
+      return false;
+  }else if(form.text.value==""){
+      alert("ご意見が入力されていません。ご意見を入力してください。")
+      return false;
+  }else{
+      return true;
+  }
+}
+
+function check(){
+  if(isFinite(form.yearolds.value==false)){
+    alert("年齢が正しく入力されていません。正しく年齢を入力してください。")
+  }
+  return false;
+}
