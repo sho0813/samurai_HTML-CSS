f8ff5b7bfba773c6042e8054e69d7e04ce36317c
sho0813 <ks4.bsk0813@gmail.com>
2020-11-17 10:28:39 +0900

    PlusHomework21

--- homework21.php
+++ homework21.php
@@ -3,7 +3,7 @@
     
     //最小値
     $mi = $x[0];
-    for ($i=1; $i<6; $i++) { 
+    for ($i=1; $i<count($x); $i++) { 
         if ($x[$i] < $mi) { 
             $mi = $x[$i];
         }
@@ -16,7 +16,7 @@
     echo $mi;
     //最大値
     $ma = $x[0];
-    for ($i=1; $i<6; $i++) { 
+    for ($i=1; $i<count($x); $i++) { 
         if ($x[$i] > $ma) { 
             $ma = $x[$i]; 
         } 
@@ -28,5 +28,21 @@
     //降順のやり方が不明です
     //ソートの仕方はいろいろとあります。
     //今回は"バブルソート"というキーワードで調べてみて対応してみてください。
-
+    //クイックソート等他にもソートがある
+    //ソートによって処理速度が変わる
+    $arr = array(60,50,90,70,80,45);
+    var_dump(bubble_sort($arr));
+    function bubble_sort($array){
+        for($i = 0; $i < count($array); $i++){
+            for($n = 1; $n < count($array); $n++){
+                if($array[$n-1] > $array[$n]){
+                    $temp = $array[$n];
+                    $array[$n] = $array[$n-1];
+                    $array[$n-1] = $temp;
+                }
+            }
+        }
+        return $array;
+    }
+    
 ?>
